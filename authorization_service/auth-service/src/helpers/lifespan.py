92b1f1d5cc1ed9a14a3bb6e337d58b1b9853fc37
bakhee <alkesey.gredyaev@gmail.com>
2024-06-23 04:12:53 +0300

    Refactor: move lifespan to the helpers/lifespan.py  auth (#6)

---
+++ authorization_service/auth-service/src/helpers/lifespan.py
@@ -0,0 +1,29 @@
+import logging
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from redis.asyncio import Redis
+
+from src.db import redis_db
+from src.models.db_entity import create_database, purge_database
+from src.helpers.jaeger import configure_tracer
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    from src.core.api_settings import settings
+
+    # On startup events
+    logging.info('Config: %s', vars(settings))
+    redis_db.redis = Redis(host=settings.redis_host, port=settings.redis_port)
+    # Creating and filling DB
+    if settings.jaeger_enable_tracer:
+        configure_tracer(
+            settings.jaeger_host,
+            settings.jaeger_port,
+            settings.service_name,
+        )
+    await create_database()
+    yield
+    # On shutdown events
+    # await purge_database()
+    await redis_db.redis.close()
