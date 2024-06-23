3b259b84a6fdc4a7f893136f63f86e5bff92f825
bakhee <alkesey.gredyaev@gmail.com>
2024-06-23 04:12:45 +0300

    Refactor: move lifespan to the helpers/lifespan.py  fastapi (#6)

---
+++ content_provider/src/helpers/lifespan.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+import logging.config
+from contextlib import asynccontextmanager
+
+from ..core.logger import LOGGING
+from ..db import elastic, redis
+from jaeger import configure_tracer
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    from ..core.config import settings
+
+    await elastic.es.info()
+    await redis.redis.initialize()
+    logging.config.dictConfig(LOGGING)
+    if settings.jaeger_enable_tracer:
+        configure_tracer(
+            settings.jaeger_host,
+            settings.jaeger_port,
+            settings.service_name,
+        )
+    yield
+    await redis.redis.close()
+    await elastic.es.close()
