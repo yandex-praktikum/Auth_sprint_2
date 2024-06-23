651aba188a54b3c24696602e1af21a5c7dee206c
bakhee <alkesey.gredyaev@gmail.com>
2024-06-23 04:18:43 +0300

    Add: UUID generation and insertion into X-Request-Id headers for tracing support in the custom backend admin (#6)

--- admin_panel/app/users/auth.py
+++ admin_panel/app/users/auth.py
@@ -1,6 +1,8 @@
 import http
 import json
+import uuid
 from logging import getLogger
+from typing import Any
 
 import backoff
 import requests
@@ -8,7 +10,8 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import BaseBackend
 from jose import jwt
-from requests.exceptions import ConnectionError, HTTPError, Timeout
+from requests import Response
+from requests.exceptions import ConnectionError, HTTPError, Timeout, RequestException
 
 from .roles import Roles
 
@@ -24,35 +27,46 @@ BACKOFF_SETTINGS = {
 
 
 class CustomBackend(BaseBackend):
+    @staticmethod
+    def _generate_request_id():
+        return str(uuid.uuid4())
+
+    def _get_request_id(self, request):
+        return request.META.get('HTTP_X_REQUEST_ID') or self._generate_request_id()
+
     @backoff.on_exception(**BACKOFF_SETTINGS)
-    def _send_request_to_login(self, payload: dict):
+    def _send_request_to_login(self, payload: dict[str, Any], headers: dict[str, Any]) -> Response:
         url = settings.AUTH_API_LOGIN_URL
-        response = requests.post(url, data=json.dumps(payload), timeout=settings.AUTH_API_TIMEOUT)
+        response = requests.post(url, data=json.dumps(payload), headers=headers, timeout=settings.AUTH_API_TIMEOUT)
         return response
 
     @backoff.on_exception(**BACKOFF_SETTINGS)
-    def _send_request_to_profile(self, user_id: str, cookies: dict):
+    def _send_request_to_profile(self, user_id: str, cookies: dict[str, Any], headers: dict[str, Any]) -> Response:
         url = f"{settings.AUTH_API_USER_INFO_URL}/{user_id}"
-        response = requests.get(url, cookies=cookies, timeout=settings.AUTH_API_TIMEOUT)
+        response = requests.get(url, cookies=cookies, headers=headers, timeout=settings.AUTH_API_TIMEOUT)
         return response
 
     def authenticate(self, request, username=None, password=None):
+        headers = {'X-Request-Id': self._get_request_id(request)}
         payload = {"email": username, "password": password}
+
         try:
-            response = self._send_request_to_login(payload)
-        except Exception:
+            response = self._send_request_to_login(payload, headers)
+        except RequestException:
             return None
         if response.status_code != http.HTTPStatus.OK:
             return None
 
         access_token = response.cookies.get(settings.ACCESS_TOKEN_COOKIE_NAME)
         claims = jwt.get_unverified_claims(access_token)
-        user, created = User.objects.get_or_create(sso_id=claims["user_id"])
+
+        user, _ = User.objects.get_or_create(sso_id=claims["user_id"])
 
         cookies = {settings.ACCESS_TOKEN_COOKIE_NAME: access_token}
+
         try:
-            response = self._send_request_to_profile(user.sso_id, cookies)
-        except Exception:
+            response = self._send_request_to_profile(user.sso_id, cookies, headers)
+        except RequestException:
             return None
         if response.status_code != http.HTTPStatus.OK:
             return None
